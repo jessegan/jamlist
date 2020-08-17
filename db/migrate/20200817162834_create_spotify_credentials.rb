740b33884ed1ed176c082c0f536883488237b696
jessegan <jesse13579@gmail.com>
2020-08-17 13:44:24 -0400

    Add id:false to SpotifyCredentials

--- db/migrate/20200817162834_create_spotify_credentials.rb
+++ db/migrate/20200817162834_create_spotify_credentials.rb
@@ -1,6 +1,6 @@
 class CreateSpotifyCredentials < ActiveRecord::Migration[6.0]
   def change
-    create_table :spotify_credentials do |t|
+    create_table :spotify_credentials, id: false do |t|
       t.integer :user_id
       t.string :token
       t.string :refresh_token
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_162834) do
+ActiveRecord::Schema.define(version: 2020_08_17_171013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "spotify_credentials", force: :cascade do |t|
+  create_table "spotify_credentials", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.string "token"
     t.string "refresh_token"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_162834) do
     t.string "display_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "spotify_id"
   end
 
 end
