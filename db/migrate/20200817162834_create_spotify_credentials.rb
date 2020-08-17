b6481ea82bbbccd40c1f76f9cb3d257035b17968
jessegan <jesse13579@gmail.com>
2020-08-17 12:30:11 -0400

    Add migration for SpotifyCredentials

---
+++ db/migrate/20200817162834_create_spotify_credentials.rb
@@ -0,0 +1,13 @@
+class CreateSpotifyCredentials < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spotify_credentials do |t|
+      t.integer :user_id
+      t.string :token
+      t.string :refresh_token
+      t.integer :expires_at
+      t.boolean :expires
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_161324) do
+ActiveRecord::Schema.define(version: 2020_08_17_162834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spotify_credentials", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.string "refresh_token"
+    t.integer "expires_at"
+    t.boolean "expires"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
