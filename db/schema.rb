b08f8a46f2805fa9afc223c839a678ad8c7e742f
jessegan <jesse13579@gmail.com>
2020-08-17 16:32:55 -0400

    Add default primary key back to SpotifyCredentials

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_08_17_171013) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "spotify_credentials", id: false, force: :cascade do |t|
+  create_table "spotify_credentials", force: :cascade do |t|
     t.integer "user_id"
     t.string "token"
     t.string "refresh_token"
