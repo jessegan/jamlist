c7316904a9e87086e4489c4446a69cbab699b98b
jessegan <jesse13579@gmail.com>
2020-08-17 16:32:13 -0400

    Add create users actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,26 @@ class UsersController < ApplicationController
         else  
          # If no, render create account form with params
             @user = User.new(email: auth_params_info['email'], spotify_id: auth_params_info['id'], image: auth_params_info['images'][0]['url'], display_name:  auth_params_info['display_name'])
-            @user.build_credential(auth_params_credentials)
+            @user.build_credential(token: auth_params_credentials[:token], refresh_token: auth_params_credentials[:refresh_token], expires_at: auth_params_credentials[:expires_at], expires: auth_params_credentials[:expires])
 
+            render 'new'
+        end
+    end
+
+    ## create
+    # Create user route
+    # Will create a user and redirect to home page
+    def create
+        binding.pry
+        @user = User.new(user_params)
+        if @user.save
+            @user.create_credential(credential_params)
+
+            session[:user_id] = @user.id
+
+            redirect_to root_path
+        else
+            flash[:error] = @user.errors.full_messages
             render 'new'
         end
     end
@@ -28,8 +46,15 @@ class UsersController < ApplicationController
     end
 
     def auth_params_credentials
-        auth_params['credentials'].permit!
+        auth_params['credentials']
     end
 
+    def user_params
+        params.require(:user).permit(:email,:display_name,:image,:spotify_id,:password,:password_confirmation)
+    end
+
+    def credential_params
+        params.require(:user).permit(credential_attributes: [:token,:refresh_token,:expires_at,:expires])
+    end
 
 end
