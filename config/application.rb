9ebcfed4f9fae1031009b1565f229bde611ab0fa
jessegan <jesse13579@gmail.com>
2020-08-17 13:53:32 -0400

    Add RSpotify authenication to config

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Jamlist
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    RSpotify::authenticate(ENV['SPOTIFY_KEY'], ENV['SPOTIFY_SECRET'])
   end
 end
