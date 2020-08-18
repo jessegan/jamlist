06504aaf8e42e2e98b95a7aa39c9a7cd79622532
jessegan <jesse13579@gmail.com>
2020-08-18 12:13:53 -0400

    Create Gruops table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_17_171013) do
+ActiveRecord::Schema.define(version: 2020_08_18_161134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "spotify_credentials", force: :cascade do |t|
     t.integer "user_id"
