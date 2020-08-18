95dde84dcf3f8945e7abf12dd7bae1489d013df9
jessegan <jesse13579@gmail.com>
2020-08-18 14:21:35 -0400

    Add helper methods to manage session requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    def require_signed_in
+        if !user_signed_in?
+            #TODO: Add flash alert
+            redirect_to login_path
+        end
+    end
+
+    def user_signed_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        @user ||= User.find(session[:user_id])
+    end
+
+    def current_spotify_user
+        @spotify_user ||= RSpotify::User.new(current_user.to_rspotify_hash)
+    end
+
 end
