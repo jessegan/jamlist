012e763ee70d5fe27dceb0fe89de1dd7a70e0a55
jessegan <jesse13579@gmail.com>
2020-08-18 09:45:53 -0400

    Create routes for login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
+  ### ROOT
   root "static#home"
 
+  ### SPOTIFY AUTH
   get "/auth/spotify/callback", to: "users#spotify"
   post "/signup", to: "users#create"
+
+  ### LOGIN & LOGOUT
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  post '/logout', to: 'sessions#logout'
+
 end
