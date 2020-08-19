1626536675805f697338bc57cc0f3ccc01cfab88
jessegan <jesse13579@gmail.com>
2020-08-19 07:59:39 -0400

    add comments

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,7 +2,14 @@ class GroupsController < ApplicationController
 
     before_action :require_signed_in
 
+    ## home
+    # Home page route
+    # Shows User's owned and joined groups
     def home
     end
+
+    
+
+
 
 end
