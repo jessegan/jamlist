364416e90f2c381b7a89a4c87b54b24c9fa7ba52
jessegan <jesse13579@gmail.com>
2020-08-19 08:19:51 -0400

    Add comments to actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,19 @@
 class SessionsController < ApplicationController
 
     ### CALLBACKS
-    
+
     before_action :redirect_if_signed_in, only: [:new,:create]
 
+    ### ACTIONS
+
+    ## new
+    # Login page
     def new
         @user = User.new
     end
 
+    ## create
+    # Checks if user exists and if password matches and then logs user in
     def create
         @user = User.find_by(email: user_params[:email])
         if @user && @user.authenticate(user_params[:password])
@@ -19,6 +25,8 @@ class SessionsController < ApplicationController
         end
     end
 
+    ## destroy
+    # Logout user
     def destroy
         session.delete :user_id
 
