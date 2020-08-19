cdf42261469a688bccffaa9b466dd89a2530d779
jessegan <jesse13579@gmail.com>
2020-08-19 10:39:00 -0400

    Add helpers methods for checking current group and member access

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,7 +3,8 @@ class GroupsController < ApplicationController
     ### CALLBACKS
 
     before_action :require_signed_in
-    before_action :set_group, only: [:show]
+
+    ### ACTIONS
 
     ## home
     # Home page route
@@ -24,10 +25,23 @@ class GroupsController < ApplicationController
     def show
     end
 
-    private
-    
-    def set_group
-        @group = Group.find(params[:id])
+    ### HELPERS
+    helper_method :current_group, :member_of
+
+    def current_group
+        @group ||= Group.find(params[:id])      
+    end
+
+    def member_of(group)
+        current_user.is_member?(group)
+    end
+
+    def require_member_of_group
+        if !member_of(current_group)
+            # TODO: Add flash alert
+            redirect_back fallback_location: home_path
+        end
     end
 
 end
+
