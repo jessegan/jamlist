6448b4ae91ea0bbfc830ca17258388377879e162
jessegan <jesse13579@gmail.com>
2020-08-19 09:15:12 -0400

    Add generate_navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+    def generate_nav_bar
+        if user_signed_in?
+            content_tag(:li, link_to("Home",home_path))
+        else
+            content_tag(:li, link_to("Home",root_path))
+        end
+    end
+
 end
