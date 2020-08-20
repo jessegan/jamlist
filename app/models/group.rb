06fa3dde6d9b920e1b139b60694e7ec96e25dda0
jessegan <jesse13579@gmail.com>
2020-08-20 13:53:12 -0400

    Add playlist association

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,6 +4,7 @@ class Group < ApplicationRecord
     has_many :members, dependent: :destroy
     has_many :users, through: :members
     belongs_to :owner, class_name: "User"
+    has_many :playlists, dependent: :destroy
 
     ### VALIDATIONS
 
