c0f17877fce634f930850da6a8b266093f965443
jessegan <jesse13579@gmail.com>
2020-08-20 15:45:50 -0400

    Add method add_to_spotify

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -8,6 +8,19 @@ class Playlist < ApplicationRecord
 
     validates :name, {presence: true, uniqueness: {scope: :group, message: "should be unique per group"}}
     validates :description, length: {maximum: 500}
-    validates :spotify_id, {presence: true, uniqueness: true}
 
+    ### INSTANCE METHODS
+
+    ## add_to_spotify
+    # adds playlist to Group owner's spotify account
+    # options include public or private and collaborative options
+    def add_to_spotify(options = {})
+        if self.spotify_id.nil?
+            playlist = self.group.owner.rspotify_user.create_playlist!(self.name)
+            self.spotify_id = playlist.id
+            self.save
+        else
+            false
+        end
+    end
 end
