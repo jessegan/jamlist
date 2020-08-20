a5a2fc7eecace122c1341f7c6e9f5823b8b82f2e
jessegan <jesse13579@gmail.com>
2020-08-20 16:05:41 -0400

    Users can create new playlists in a group and see them on their spotify accounts

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,2 +1,45 @@
 class PlaylistsController < ApplicationController
+
+    ### HELPERS
+
+    helper_method :current_group
+
+    ### CALLBACKS
+
+    
+    ### ACTIONS
+
+    ##new
+    # new playlist route
+    # renders form to create new playlist
+    def new
+        @playlist = current_group.playlists.build()
+    end
+
+    ##create
+    # create playlist route
+    # handles creation of new playist
+    def create
+        @playlist = current_group.playlists.build(playlist_params)
+        if @playlist.save
+            @playlist.add_to_spotify
+
+            redirect_to group_playlist_path(current_group,@playlist)
+        else  
+            # TODO: add flash error
+            render :new
+        end
+    end
+
+
+    private 
+
+    def current_group
+        @group ||= Group.find(params[:group_id])
+    end
+
+    def playlist_params
+        params.require(:playlist).permit(:name,:description)
+    end
+
 end
