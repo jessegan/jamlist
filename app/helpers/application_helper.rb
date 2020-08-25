7ed6dc4ac9595a3fb85f7020b84fc3fdbcc6a3f2
jessegan <jesse13579@gmail.com>
2020-08-25 12:40:09 -0400

    Update generate_nav_bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 
     def generate_nav_bar
         if user_signed_in?
-            @content = content_tag(:li, link_to("Home",home_path))
-            @content << content_tag(:li, link_to("Profile",current_user))
+            @content = content_tag(:li, link_to("Home",home_path, class: "black-text"))
+            @content << content_tag(:li, link_to("Profile",current_user, class: "black-text"))
         else
-            content_tag(:li, link_to("Home",root_path))
+            content_tag(:li, link_to("Login","/login", class: "black-text"))
         end
     end
 
